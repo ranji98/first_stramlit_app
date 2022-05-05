75dc33c7b1460417860dafd8721405cc51795b13
ranji98 <104899697+ranji98@users.noreply.github.com>
2022-05-05 12:41:29 +0530

    Update streamlit_app.py
    displaying the selected fruites

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,14 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
+#selecting the fruites by using numbers
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+streamlit.dataframe(my_fruit_list)
+
+#displaying the fruites which is selected 
+import pandas 
+My_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
