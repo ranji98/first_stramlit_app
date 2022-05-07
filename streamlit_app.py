5cf5bb82acab4e21b48e68d2e0a45c9a87189750
ranji98 <104899697+ranji98@users.noreply.github.com>
2022-05-07 12:15:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
+fruitvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruitvice_response.json())--------------------to delete the line
+fruitvice_normalized=pandas.json_normalize(fruitvice_response.json())
+streamlit.dataframe(fruitvice_normalized)
+
+
 streamlit.header('fruityvice fruit Advice!')
 fruit_choice = streamlit.text_input('what fruit would you like information about?','Kiwi')
 streamlit.write('The user entered', fruit_choice)
