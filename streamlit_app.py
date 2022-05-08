6af6a8e0fcfb5f9aa218a06a12f3b1b98b7eda7a
ranji98 <104899697+ranji98@users.noreply.github.com>
2022-05-08 15:38:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,26 @@
 import streamlit
-streamlit.title('My Mom\'s New Healthy Diner')
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title("My Mom's new Healthy Diner")
+
 streamlit.header('Breakfast Favorites')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
-
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-#let's pick a fruit list here so they can pick fruit they want to include
-fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-#display the table on the page
-streamlit.dataframe(fruits_to_show)
-
-#import requests
-#fruitvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruitvice_response.json())--------------------to delete the line
-#fruitvice_normalized=pandas.json_normalize(fruitvice_response.json())
-#streamlit.dataframe(fruitvice_normalized)
-
-
-streamlit.header('fruityvice fruit Advice!')
-fruit_choice = streamlit.text_input('what fruit would you like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice)
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruitvice_normalized=pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruitvice_normalized)
-
+#let's put a pick list here so they can pick the fruit they want to include
+fruit_selected = streamlit.multiselect("pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruit_to_show = my_fruit_list.loc[fruit_selected]
+#Display the Table on the page.  
+streamlit.dataframe(fruit_to_show)
 
 
 
